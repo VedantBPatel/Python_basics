a12c885dc3a49997c3853f1707afbd7467e9d38c
VedantPatel <vedantpatel1969@gmail.com>
2025-03-17 22:58:18 +0530

    Operators

---
+++ Operators.py
@@ -0,0 +1,24 @@
+a=9
+b=4
+
+#Arithmetic Operators
+
+#Addition
+add=a+b
+#Subtraction
+sub=a-b
+#Multiplication
+mul=a*b
+#Division(float)
+div1=a/b
+#Division(floor)
+div2=a//b
+#Modulo
+mod=a%b
+
+print(add)
+print(sub)
+print(mul)
+print(div1)
+print(div2)
+print(mod)
