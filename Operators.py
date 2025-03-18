1ed1bc53adbd4669cc4689cefeb1459a48aa0633
VedantPatel <vedantpatel1969@gmail.com>
2025-03-18 20:43:16 +0530

    Operators

--- Operators.py
+++ Operators.py
@@ -22,3 +22,65 @@ print(mul)
 print(div1)
 print(div2)
 print(mod)
+
+
+#Relational Operators
+a=13
+b=33
+
+#Greater than 
+print(a>b)
+
+#Less than
+print(a<b)
+
+#Equals to
+print(a==b)
+
+#Not equal to
+print(a!=b)
+
+#Greater than or Equal to
+print(a>=b)
+
+#Less than or Equal to
+print(a<=b)
+
+#Logical operators
+a=True
+b=False
+
+print(a and b)#prints a and b is false
+print(a or b)#prints a or b is true
+print(not a)#prints not a is false
+
+
+#Bitwise Operator
+a=10
+b=4
+print(a & b )#and
+print(a | b)#or
+print(~a)#complement
+print(a ^ b)#xor
+print(a>>2)#right shift
+print(a<<2)#left shift
+
+#Python has additional 2 operators: membership and identity
+a=15
+b=10
+list=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+#membership operators
+if(a in list):
+    print(f"{a} is present in list")
+else:
+    print(f"{a} is not present in list")
+
+if(b not in list):
+    print(f"{b}is not present in list")
+else:
+    print(f"{b} is present in list")
+
+
+#identity operators
+print(a is 15)
+print(a is not 15)
