1d46712e14c7bc0934033ba459c91b4ec98ec38d
VedantPatel <vedantpatel1969@gmail.com>
2025-03-19 21:36:30 +0530

    List and Basic Operations

---
+++ Python List.py
@@ -0,0 +1,53 @@
+#1: Creating a List
+list=[1,2,3,4,5,"Vedant","Rahul"]
+print(list)
+
+#2: Accessing List Elements
+print(list[0])
+
+#3: Adding Elements into List
+"""
+In list we can add elements in total 3 ways
+1.append():Adds and elements at the end of the list.
+2.extend():Adds multiple elements at the end of the list.
+3.insert():Adds an element at a specific position.
+"""
+
+a=[]
+a.append(10)
+print("after append(10):", a) 
+a.extend([15,20,25,30])
+print("after extend():",a )
+a.insert(0,5)#insert(index,value)
+print("after insert(0,5):",a)     
+
+#4: Updating Elements in Lists
+list=[10,15,20,25,30]
+list[1]=25
+print(list)
+
+#5: Removing Elements in Lists
+"""
+Just like the addition we can remove the elements in 3 different ways.
+1.remove():Removes the first occurence of an element.
+2.pop():Removes the element at a specific index or the last element if no index is specified.
+3.del:Deletes an element at a spcified index.
+"""
+list=["Figma","Steve",1,2,3,"Robin","Jack"]
+list.remove("Figma")
+print( "After removing Figma:",list)
+list.pop()
+print("After using pop without providing any index",list)
+list.pop(2)
+print("After providing index to pop(2)",list)
+del list[0]
+print("After deletion list[0]:",list)
+del list
+print(list)
+
+#6: Iterating over List
+list=["Vedant",1,2,"Josh","David",'s','p','d']
+for i in list:
+    print(i)
+for i in list:
+    print(i,end=" ")
